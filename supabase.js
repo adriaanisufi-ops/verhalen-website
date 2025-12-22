0d4b119a71402ffb607b2c6aa7523a6e5463390a
adriaanisufi-ops <adriaanisufi@gmail.com>
2025-12-22 14:17:35 +0100

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,10 @@
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
+<script>
+  const SUPABASE_URL = "https://mzlvftetsjnpbnqncwzg.supabase.co";
+  const SUPABASE_KEY = "sb_publishable_6JUFHg9mRZHA0iOiDqI0vA_YaF4dvaY";
+
+  const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+  );
+</script>
